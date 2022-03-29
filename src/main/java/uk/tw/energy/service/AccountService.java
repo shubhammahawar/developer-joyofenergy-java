3344eaa8964d253ec24a872423c1c29605f919b4
Shubham <shubham.mahawar1001@gmail.com>
2022-03-29 11:45:24 +0530

    added comment from intellij

--- src/main/java/uk/tw/energy/service/AccountService.java
+++ src/main/java/uk/tw/energy/service/AccountService.java
@@ -10,6 +10,8 @@ public class AccountService {
     // updating account file
     //new check changes
 
+//changes done from intellij
+
     private final Map<String, String> smartMeterToPricePlanAccounts;
 
     public AccountService(Map<String, String> smartMeterToPricePlanAccounts) {
